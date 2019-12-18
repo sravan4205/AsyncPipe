f991a3f02752e243c5529820e4e81200a44b6c82
sravan4205 <sravan.amishetti@gmail.com>
2019-12-18 17:44:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Observable } from 'rxjs'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'AsyncPipe';
+  observable: Observable<number>
+
+  ngOnInit() {
+    this.observable = Observable.create(observer => {
+      let value = 0
+      const interval = setInterval(() => {
+        observer.next(value)
+        value++
+      }, 1000)
+
+      return () => clearInterval(interval)
+    })
+  }
 }
